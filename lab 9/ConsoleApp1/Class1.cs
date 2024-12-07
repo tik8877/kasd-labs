d3ac2a4c382ecd75ac27d22b8970040d9e188af9
tik8877 <tik88755@gmail.com>
2024-12-07 16:02:12 +0300

    Add files via upload

---
+++ lab 9/ConsoleApp1/Class1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+
+namespace Vector
+{
+    internal class MyVector<T>
+    {
+        protected T[] elementData;
+        protected int elementCount;
+        protected int capacityIncrement;
+
+        // Конструкторы
+        public MyVector(int initialCapacity, int capacityIncrement)
+        {
+            if (initialCapacity < 0)
+                throw new ArgumentOutOfRangeException(nameof(initialCapacity), "Capacity must be non-negative.");
+
+            elementData = new T[initialCapacity];
+            elementCount = 0;
+            this.capacityIncrement = capacityIncrement;
+        }
+
+        public MyVector(int initialCapacity) : this(initialCapacity, 0) { }
+
+        public MyVector()
+        {
+            elementData = new T[10];
+            elementCount = 0;
+            capacityIncrement = 0;
+        }
+
+        public MyVector(T[] array)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array));
+
+            elementData = new T[array.Length];
+            Array.Copy(array, elementData, array.Length);
+            elementCount = array.Length;
+            capacityIncrement = 0;
+        }
+
+        // Методы
+        public void Add(T element)
+        {
+            EnsureCapacity(elementCount + 1);
+            elementData[elementCount++] = element;
+        }
+
+        public void AddAll(T[] array)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array));
+
+            EnsureCapacity(elementCount + array.Length);
+            Array.Copy(array, 0, elementData, elementCount, array.Length);
+            elementCount += array.Length;
+        }
+
+        public void Clear()
+        {
+            elementCount = 0;
+            elementData = new T[10];
+        }
+
+        public bool Contains(T element)
+        {
+            for (int i = 0; i < elementCount; i++)
+            {
+                if (Equals(elementData[i], element))
+                    return true;
+            }
+            return false;
+        }
+
+        public bool IsEmpty() => elementCount == 0;
+
+        public void Remove(T element)
+        {
+            int index = IndexOf(element);
+            if (index >= 0)
+                RemoveAt(index);
+        }
+
+        public void RemoveAll(T[] array)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array));
+
+            foreach (var item in array)
+                Remove(item);
+        }
+
+        public int IndexOf(T element)
+        {
+            for (int i = 0; i < elementCount; i++)
+            {
+                if (Equals(elementData[i], element))
+                    return i;
+            }
+            return -1;
+        }
+
+        public T Get(int index)
+        {
+            ValidateIndex(index);
+            return elementData[index];
+        }
+
+        public void Set(int index, T element)
+        {
+            ValidateIndex(index);
+            elementData[index] = element;
+        }
+
+        public void RemoveAt(int index)
+        {
+            ValidateIndex(index);
+            for (int i = index; i < elementCount - 1; i++)
+                elementData[i] = elementData[i + 1];
+
+            elementData[elementCount - 1] = default;
+            elementCount--;
+        }
+
+        public void RemoveRange(int begin, int end)
+        {
+            if (begin < 0 || end > elementCount || begin >= end)
+                throw new ArgumentOutOfRangeException("Invalid range specified.");
+
+            int range = end - begin;
+            for (int i = begin; i < elementCount - range; i++)
+                elementData[i] = elementData[i + range];
+
+            Array.Clear(elementData, elementCount - range, range);
+            elementCount -= range;
+        }
+
+        public int Size() => elementCount;
+
+        public T[] ToArray()
+        {
+            T[] result = new T[elementCount];
+            Array.Copy(elementData, result, elementCount);
+            return result;
+        }
+
+        public MyVector<T> SubList(int fromIndex, int toIndex)
+        {
+            if (fromIndex < 0 || toIndex > elementCount || fromIndex >= toIndex)
+                throw new ArgumentOutOfRangeException("Invalid sublist range.");
+
+            T[] subArray = new T[toIndex - fromIndex];
+            Array.Copy(elementData, fromIndex, subArray, 0, toIndex - fromIndex);
+            return new MyVector<T>(subArray);
+        }
+
+        // Приватные методы
+        private void EnsureCapacity(int minCapacity)
+        {
+            if (elementData.Length < minCapacity)
+            {
+                int newCapacity = capacityIncrement > 0
+                    ? elementData.Length + capacityIncrement
+                    : elementData.Length * 2;
+
+                if (newCapacity < minCapacity)
+                    newCapacity = minCapacity;
+
+                Array.Resize(ref elementData, newCapacity);
+            }
+        }
+
+        private void ValidateIndex(int index)
+        {
+            if (index < 0 || index >= elementCount)
+                throw new ArgumentOutOfRangeException(nameof(index), "Index is out of range.");
+        }
+    }
+
+    public class MyStack<T>
+    {
+        private MyVector<T> stack;
+
+        public MyStack()
+        {
+            stack = new MyVector<T>();
+        }
+
+        public void Push(T item) => stack.Add(item);
+
+        public T Pop()
+        {
+            if (stack.IsEmpty())
+                throw new InvalidOperationException("Stack is empty.");
+
+            T item = stack.Get(stack.Size() - 1);
+            stack.RemoveAt(stack.Size() - 1);
+            return item;
+        }
+
+        public T Peek()
+        {
+            if (stack.IsEmpty())
+                throw new InvalidOperationException("Stack is empty.");
+
+            return stack.Get(stack.Size() - 1);
+        }
+
+        public bool IsEmpty() => stack.IsEmpty();
+
+        public int Search(T item)
+        {
+            int index = stack.IndexOf(item);
+            return index >= 0 ? stack.Size() - index : -1;
+        }
+
+        public void Print()
+        {
+            for (int i = stack.Size() - 1; i >= 0; i--)
+                Console.WriteLine(stack.Get(i));
+        }
+    }
+}
---
+++ lab 9/ConsoleApp1/Program.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using Vector;
+using System.Collections.Generic;
+using System.Numerics;
+namespace Lab9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите математическое выражение: ");
+            string primer = Console.ReadLine();
+            Console.WriteLine(Calc(primer));
+        }
+
+        static private double InitOperaci(string operation, params double[] x) => operation switch
+        {
+            "+" => x[0] + x[1],
+            "-" => x[0] - x[1],
+            "*" => x[0] * x[1],
+            "/" => x[0] / x[1],
+            "^" => Math.Pow(x[0], x[1]),
+            "koren" => Math.Sqrt(x[0]),
+            "abs" => Math.Abs(x[0]),
+            "sin" => Math.Sin(x[0]),
+            "cos" => Math.Cos(x[0]),
+            "tg" => Math.Tan(x[0]),
+            "ln" => Math.Log(x[0]),
+            "log" => Math.Log10(x[0]),
+            "min" => x[0] < x[1] ? x[0] : x[1],
+            "max" => x[0] > x[1] ? x[0] : x[1],
+            "mod" => (int)x[1] % (int)x[0],
+            "//" => Math.Floor(x[0] / x[1]),
+            "exp" => Math.Exp(x[0]),
+            "trunc" => Math.Truncate(x[0]),
+            _ => throw new Exception("Неизвестная операция")
+        };
+
+        static public int Prioritet(string operation)
+        {
+            switch (operation)
+            {
+                case "+":
+                case "-":
+                    return 1;
+                case "*":
+                case "/":
+                case "//":
+                    return 2;
+                case "^":
+                    return 3;
+                case "koren":
+                case "ln":
+                case "cos":
+                case "sin":
+                case "tg":
+                case "abs":
+                case "log":
+                case "min":
+                case "max":
+                case "mod":
+                case "exp":
+                case "trunc":
+                    return 4;
+                default: return 0;
+            }
+        }
+
+        static private string Tmp(string expression)
+        {
+            MyVector<string> vectorOfItem = new MyVector<string>();
+            for (int i = 0; i < expression.Length; i++)
+            {
+                string item = "";
+                while (i < expression.Length && Char.IsLetter(expression[i]))
+                {
+                    item += expression[i];
+                    i++;
+                }
+                if (item.Length > 0)
+                {
+                    switch (item)
+                    {
+                        case "koren":
+                        case "ln":
+                        case "cos":
+                        case "sin":
+                        case "tg":
+                        case "abs":
+                        case "log":
+                        case "min":
+                        case "max":
+                        case "mod":
+                        case "exp":
+                        case "trunc":
+                            break;
+                        default:
+                            vectorOfItem.Add(item);
+                            break;
+                    }
+                }
+            }
+
+            for (int i = 0; i < vectorOfItem.Size(); i++)
+            {
+                Console.WriteLine($"Введите значение {vectorOfItem.Get(i)}: ");
+                expression = expression.Replace(vectorOfItem.Get(i), Console.ReadLine());
+            }
+            return expression;
+        }
+
+        static public MyVector<string> PolskNotacia(string expression)
+        {
+            expression = Tmp(expression);
+            MyStack<string> stack = new MyStack<string>();
+            MyVector<string> result = new MyVector<string>();
+
+            char[] basicOperations = new char[] { '+', '-', '*', '^', '/' };
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                string number = "";
+                if ((result.IsEmpty() && expression[i] == '-') || (i > 0 && expression[i] == '-' && expression[i - 1] == '('))
+                {
+                    number += expression[i];
+                    i++;
+                }
+                while (i < expression.Length && (Char.IsDigit(expression[i]) || expression[i] == '.'))
+                {
+                    number += expression[i];
+                    i++;
+                }
+                if (number.Length > 0) { result.Add(number); }
+
+                string func = "";
+                while (i < expression.Length && Char.IsLetter(expression[i]))
+                {
+                    func += expression[i];
+                    i++;
+                }
+                if (func.Length > 0 && func == "pi") { result.Add(Math.PI.ToString()); }
+
+                else if (func.Length > 0) { stack.Push(func); }
+
+                if (i < expression.Length && basicOperations.Contains(expression[i]))
+                {
+                    if (stack.IsEmpty()) { stack.Push(expression[i].ToString()); }
+                    else
+                    {
+                        while (!stack.IsEmpty() && (Prioritet(stack.Peek()) > Prioritet(expression[i].ToString())))
+                        {
+                            string b = stack.Peek();
+                            result.Add(b.ToString());
+                            stack.Pop();
+                        }
+                        stack.Push(expression[i].ToString());
+                    }
+                }
+                else if (i < expression.Length && expression[i] == '(') stack.Push(expression[i].ToString());
+                else if (i < expression.Length && expression[i] == ')')
+                {
+                    while (!stack.IsEmpty())
+                    {
+                        string b = stack.Peek();
+                        if (b == "(")
+                        {
+                            stack.Pop();
+                            break;
+                        }
+                        result.Add(b.ToString());
+                        stack.Pop();
+                    }
+                }
+            }
+            while (!stack.IsEmpty())
+            {
+                string b = stack.Peek();
+                if (b != "(") result.Add(b.ToString());
+                if (b == ")") throw new Exception("Количество скобок не совпадает");
+                stack.Pop();
+            }
+
+            return result;
+        }
+
+        static public double Calc(string expression)
+        {
+            if (expression == null) throw new Exception("Пусто");
+            MyVector<string> postfixForm = PolskNotacia(expression);
+            MyStack<double> stack = new MyStack<double>();
+
+            char[] basicOperator = new char[] { '+', '-', '*', '^', '/' };
+
+            for (int i = 0; i < postfixForm.Size(); i++)
+            {
+                string element = postfixForm.Get(i);
+                if (Char.IsDigit(element[0]) || (element.Length > 1 && Char.IsDigit(element[1])))
+                {
+                    element = element.Replace('.', ',');
+                    double number = Convert.ToDouble(element);
+                    stack.Push(number);
+                }
+                else if (Char.IsLetter(element[0]))
+                {
+                    if (element == "max" || element == "min" || element == "mod")
+                    {
+                        double number1 = stack.Peek();
+                        stack.Pop();
+                        double number2 = stack.Peek();
+                        stack.Pop();
+                        stack.Push(InitOperaci(element, number1, number2));
+                    }
+                    else
+                    {
+                        double number = stack.Peek();
+                        stack.Pop();
+                        stack.Push(InitOperaci(element, number));
+                    }
+                }
+                else if (basicOperator.Contains(element[0]))
+                {
+                    if (i < postfixForm.Size() - 1 && element == "/" && postfixForm.Get(i + 1) == "/")
+                    {
+                        i++;
+                        element += postfixForm.Get(i);
+                    }
+                    double number2 = stack.Peek();
+                    stack.Pop();
+                    double number1 = stack.Peek();
+                    stack.Pop();
+                    stack.Push(InitOperaci(element, number1, number2));
+                }
+            }
+            double answer = stack.Peek();
+            stack.Pop();
+            return answer;
+        }
+    }
+}
