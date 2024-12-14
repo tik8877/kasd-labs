f4dce3566266c9886cd0065fa634443e7caad5c7
tik8877 <tik88755@gmail.com>
2024-12-14 09:43:38 +0300

    Add files via upload

---
+++ lab 11/ConsoleApp1/Class1.cs
@@ -0,0 +1,203 @@
+﻿namespace MyArrayList_Class
+{
+    public class MyArrayList<T>
+    {
+        private T[] elementData;
+        private int size;
+
+        public MyArrayList()
+        {
+            size = 0;
+            elementData = new T[0];
+        }
+        public MyArrayList(T[] a)
+        {
+            elementData = new T[a.Length];
+            for (int i = 0; i < elementData.Length; i++)
+            {
+                elementData[i] = a[i];
+            }
+            size = a.Length;
+        }
+        public MyArrayList(int capacity)
+        {
+            elementData = new T[capacity];
+            size = 0;
+        }
+        public void Add(T e)
+        {
+            if (size == elementData.Length)
+            {
+                T[] NewElementData = new T[size + 1 + (size / 2)];
+                for (int i = 0; i < size; i++)
+                {
+                    NewElementData[i] = elementData[i];
+                }
+                elementData = NewElementData;
+            }
+            elementData[size] = e;
+            size++;
+        }
+        public void AddAll(T[] a)
+        {
+            foreach (var smth in a)
+            {
+                Add(smth);
+            }
+        }
+        public void Clear()
+        {
+            size = 0;
+        }
+        public bool Contains(object o)
+        {
+            for (int i = 0; i < size; i++)
+                if (Equals(o, elementData[i])) return true;
+            return false;
+        }
+        public bool ContainsAll(T[] a)
+        {
+            foreach (var smth in a)
+                if (!Contains(smth)) return false;
+            return true;
+        }
+        public bool isEmpty()
+        {
+            if (size == 0) return true;
+            else return false;
+        }
+        public void Remove(object o)
+        {
+            for (int i = 0; i < size; i++)
+                if (Equals(elementData[i], o))
+                {
+                    for (int j = i; j < size - 1; j++)
+                        elementData[j] = elementData[j + 1];
+                    size--;
+                }
+        }
+        public void RemoveAll(T[] a)
+        {
+            for (int k = 0; k < a.Length; k++)
+            {
+                Remove(a[k]);
+            }
+        }
+        public void RetainAll(T[] a)
+        {
+            int newSize = 0;
+            T[] newElementData = new T[size];
+            for (int k = 0; k < a.Length; k++)
+            {
+                for (int i = 0; i < size; i++)
+                    if (Equals(a[k], elementData[i]))
+                    {
+                        newElementData[newSize] = a[k];
+                        newSize++;
+                    }
+            }
+            size = newSize;
+            elementData = newElementData;
+        }
+        public int Size()
+        {
+            return size;
+        }
+        public T[] ToArray()
+        {
+            T[] result = new T[size];
+            Array.Copy(elementData, result, size);
+            return result;
+        }
+        public T[] ToArray(ref T[] a)
+        {
+            if (a == null) a = ToArray();
+            Array.Copy(elementData, a, size);
+            return a;
+        }
+        public void Add(int index, T e)
+        {
+            if (index > size) { Add(e); return; }
+            T[] NewElementData = new T[size + 1];
+            for (int i = 0; i < index; i++)
+            {
+                NewElementData[i] = elementData[i];
+            }
+            NewElementData[index] = e;
+            for (int i = index + 1; i < size; i++)
+            {
+                NewElementData[i] = elementData[i - 1];
+            }
+            elementData = NewElementData;
+            size++;
+        }
+        public void AddAll(int index, T[] a)
+        {
+            if (index > size) { AddAll(a); return; }
+            T[] NewElementData = new T[size + a.Length];
+            for (int i = 0; i < index; i++)
+            {
+                NewElementData[i] = elementData[i];
+            }
+            for (int i = 0; i < a.Length; i++)
+            {
+                NewElementData[i + index] = a[i];
+            }
+            for (int i = index; i < size; i++)
+            {
+                NewElementData[i + index] = elementData[i];
+            }
+            elementData = NewElementData;
+            size += a.Length;
+        }
+        public T Get(int index)
+        {
+            if (index < 0 || index > size)
+                throw new ArgumentOutOfRangeException("index");
+            return elementData[index];
+        }
+        public int IndexOf(object o)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (Equals(o, elementData[i])) return i;
+            }
+            return -1;
+        }
+        public int LastIndexOf(object o)
+        {
+            for (int i = size - 1; i >= 0; i--)
+            {
+                if (Equals(o, elementData[i])) return i;
+            }
+            return -1;
+        }
+        public T Remove(int index)
+        {
+            if (index < 0 || index > size)
+                throw new ArgumentOutOfRangeException("index");
+            T element = elementData[index];
+            for (int i = index + 1; i < size; i++)
+            {
+                elementData[i - 1] = elementData[i];
+            }
+            size--;
+            return element;
+        }
+        public void Set(int index, T e)
+        {
+            if (index < 0 || index > size)
+                throw new ArgumentOutOfRangeException("index");
+            elementData[index] = e;
+        }
+        public T[] SubList(int fromindex, int toindex)
+        {
+            if ((fromindex < 0 || fromindex > size) || (toindex < 0 || toindex > size))
+                throw new ArgumentOutOfRangeException("fromindex", "toindex");
+            T[] Result = new T[toindex - fromindex];
+            for (int i = fromindex; i < toindex; i++)
+                Result[i - fromindex] = elementData[i];
+            return Result;
+        }
+    }
+}
---
+++ lab 11/ConsoleApp1/Program.cs
@@ -0,0 +1,250 @@
+﻿
+using MyArrayList_Class;
+public class MyPriorityQueue<T> where T : IComparable<T>
+{
+    private MyArrayList<T> queue;
+    private int size;
+    private Comparer<T> comparator;
+
+    public MyPriorityQueue() // конструктор для создания пустой очереди с приоритетами
+    {
+        queue = new MyArrayList<T>(11);
+        size = 0;
+        comparator = Comparer<T>.Default;
+    }
+
+    public void Queuelify(int index)
+    {
+        int left = 2 * index + 1;
+        int right = 2 * index + 2;
+        int parent = index;
+
+        if (left < size && comparator.Compare(queue.Get(left), queue.Get(parent)) > 0)
+        {
+            parent = left;
+        }
+
+        if (right < size && comparator.Compare(queue.Get(right), queue.Get(parent)) > 0)
+        {
+            parent = right;
+        }
+
+        if (parent != index)
+        {
+            Swap(index, parent);
+            Queuelify(parent);
+        }
+    }
+
+    public void Swap(int index1, int index2) // конструктор  для создания пустой очереди с приоритетами
+    {
+        T temp1 = queue.Get(index1);
+        T temp2 = queue.Get(index2);
+        queue.Set(index2, temp1);
+        queue.Set(index1, temp2);
+    }
+
+    public MyPriorityQueue(T[] a) // конструктор для создания очереди с приоритетами
+    {
+        queue = new MyArrayList<T>(a.Length);
+        size = a.Length;
+        comparator = Comparer<T>.Default;
+        for (int i = 0; i < size; i++)
+        {
+            queue.Add(a[i]);
+        }
+        for (int i = size / 2; i >= 0; i--)
+        {
+            Queuelify(i);
+        }
+    }
+
+    public MyPriorityQueue(int initialCcapacity) // конструктор для создания пустой очереди с приоритетами с указанной начальной ёмкостью
+    {
+        queue = new MyArrayList<T>(initialCcapacity);
+        size = 0;
+        comparator = Comparer<T>.Default;
+    }
+
+    public MyPriorityQueue(int initialCcapacity, Comparer<T> comparator) // конструктор для создания пустой очереди с приоритетами с указанной начальной ёмкостью и компаратором
+    {
+        queue = new MyArrayList<T>(initialCcapacity);
+        size = initialCcapacity;
+        this.comparator = comparator;
+    }
+
+    public MyPriorityQueue(MyPriorityQueue<T> c) // конструктор для создания очереди с приоритетами, содержащей элементы указанной очереди с приоритетами
+    {
+        queue = new MyArrayList<T>(c.size);
+        size = c.size;
+        comparator = c.comparator;
+        for (int i = 0; i < c.size; i++)
+        {
+            queue.Add(c.queue.Get(i));
+        }
+        size = c.size;
+
+        for (int i = size / 2; i >= 0; i--)
+        {
+            Queuelify(i);
+        }
+    }
+
+    public void Add(T e) // метод для добавления элемента в конец очереди с приоритетами
+    {
+        queue.Add(e);
+        size++;
+        for (int i = size / 2; i >= 0; i--)
+        {
+            Queuelify(i);
+        }
+    }
+
+    public void AddAll(T[] a) // метод для добавления элементов из массива
+    {
+        queue.AddAll(a);
+        size += a.Length;
+        for (int i = size / 2; i >= 0; i--)
+        {
+            Queuelify(i);
+        }
+    }
+
+    public void Clear() // метод для удаления всех элементов из очереди с приоритетами
+    {
+        queue.Clear();
+        size = 0;
+    }
+
+    public bool Contains(object o) // метод для проверки, находится ли указанный объект в очереди с приоритетами
+    {
+        return queue.Contains(o);
+    }
+
+    public bool ContainsAll(T[] a) // метод для  проверки, содержатся ли указанные объекты в очереди с приоритетами
+    {
+        return queue.ContainsAll(a);
+    }
+
+    public bool IsEmpty() // метод для проверки, является ли очередь с приоритетами пустой
+    {
+        return queue.isEmpty();
+    }
+
+    public void Remove(object o) // метод для удаления указанного объекта из очереди с приоритетами, если он есть там
+    {
+        queue.Remove(o);
+        size--;
+        for (int i = size / 2; i >= 0; i--)
+        {
+            Queuelify(i);
+        }
+    }
+
+    public void RemoveAll(T[] a) // метод для удаления указанных объектов из очереди с приоритетами
+    {
+        queue.RemoveAll(a);
+        size -= a.Length;
+        for (int i = size / 2; i >= 0; i--)
+        {
+            Queuelify(i);
+        }
+    }
+
+    public void RetainAll(T[] a) // метод для оставления в очереди с приоритетами только указанных объектов
+    {
+        queue.RetainAll(a);
+        size = a.Length;
+        for (int i = size / 2; i >= 0; i--)
+        {
+            Queuelify(i);
+        }
+    }
+
+    public int Size() // метод для получения размера очереди с приоритетами в элементах.
+    {
+        return queue.Size();
+    }
+
+    public T[] ToArray() // метод для возвращения массива объектов, содержащего все элементы очереди с приоритетами
+    {
+        return queue.ToArray();
+    }
+
+    public T[] ToArray(T[] a) // метод для возвращения массива объектов, содержащего все элементы очереди с приоритетами
+
+    {
+        return queue.ToArray( ref a);
+    }
+
+    public T Element() // метод для возвращения элемента из головы очереди с приоритетами без его удаления
+    {
+        return queue.Get(0);
+    }
+
+    public bool Offer(T obj) // метод для попытки добавления элемента obj в очередь с приоритетами. Возвращает true если добавлен и false если нет
+    {
+        queue.Add(obj);
+        size++;
+        for (int i = size / 2; i >= 0; i--)
+        {
+            Queuelify(i);
+        }
+
+        bool f = true;
+        if (queue.Contains(obj)) return f;
+        else return f == false;
+    }
+
+    public T Peek() // метод для возврата элемента из головы очереди с приоритетами без его удаления
+    {
+        if (size == 0) throw new ArgumentOutOfRangeException();
+        else return queue.Get(0);
+    }
+
+    public T Poll() // метод для удаления и возврата элемента из головы очереди с приоритетами
+    {
+        if (size == 0) throw new ArgumentOutOfRangeException();
+        else
+        {
+            T element = queue.Get(0);
+            queue.Remove(element);
+            size--;
+            for (int i = size / 2; i >= 0; i--)
+            {
+                Queuelify(i);
+            }
+            return element;
+        }
+    }
+
+    public void Print()
+    {
+        for (int i = 0; i < size; i++)
+            Console.WriteLine(queue.Get(i));
+        Console.WriteLine();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Введите элементы :");
+        int[] array = new int[8];
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.WriteLine();
+        MyPriorityQueue<int> queue = new MyPriorityQueue<int>(array);
+        queue.Print();
+        Console.WriteLine(queue.Offer(30));
+        queue.Print();
+        Console.WriteLine("Удаленный и возвращенный элемент головы");
+        Console.WriteLine(queue.Poll());
+        Console.WriteLine();
+        queue.Print();
+    }
+}
