ffa6b2a1eeea00847165a42821b53bd5bdec6859
tik8877 <tik88755@gmail.com>
2024-12-22 20:42:02 +0300

    Add files via upload

---
+++ lab 23/ConsoleApp5/Class1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using lab18;
+
+namespace lab23
+{
+
+    public class MyHashSet<T> where T : IComparable
+    {
+        MyHashMap<T, T> map = new MyHashMap<T, T>();
+        int size;
+        double loadfactor;
+
+        public MyHashSet()
+        {
+            map = new MyHashMap<T, T>(16);
+            size = 0;
+            loadfactor = 0.75;
+        }
+
+        public MyHashSet(int initialCapacity)
+        {
+            map = new MyHashMap<T, T>(initialCapacity);
+            size = 0;
+            loadfactor = 0.75;
+        }
+
+        public MyHashSet(int initialCapacity, float loadFactor)
+        {
+            map = new MyHashMap<T, T>(initialCapacity, loadFactor);
+            size = 0;
+            loadfactor = loadFactor;
+        }
+
+        public void Print() => map.KeySetPrint();
+
+
+        public void Clear() { size = 0; map = new MyHashMap<T, T>(16); }
+
+        public void Add(T elem)
+        {
+            if (Contains(elem))
+                return;
+            map.Put(elem, elem);
+            size = map.Size();
+        }
+
+        public void AddAll(T[] elems)
+        {
+            foreach (var elem in elems)
+                Add(elem);
+        }
+
+
+        public bool Contains(T elem)
+        {
+            var value = map.Get(elem);
+            return !EqualityComparer<T>.Default.Equals(value, default);
+        }
+
+        public bool[] ContainsAll(T[] elems)
+        {
+            bool[] boolElems = new bool[elems.Length];
+            int i = 0;
+            foreach (var elem in elems)
+            {
+                if (Contains(elem))
+                    boolElems[i] = true;
+                else
+                    boolElems[i] = false;
+                i++;
+            }
+            return boolElems;
+        }
+
+        public void RemoveAll(T[] elems)
+        {
+            foreach (var elem in elems)
+            {
+                if (Contains(elem))
+                {
+                    map.Remove(elem);
+                    size = map.Size();
+                }
+            }
+        }
+
+        public void RetainAll(T[] elems)
+        {
+            T[] keys = map.KeySet();
+            foreach (var key in keys)
+            {
+                foreach (var elem in elems)
+                {
+                    if (!key.Equals(elem))
+                    {
+                        map.Remove(key);
+                        size = map.Size();
+                    }
+                }
+
+            }
+        }
+
+        public T[] ToArray() => map.KeySet();
+
+        public T? First()
+        {
+            if (IsEmpty())
+                return default;
+            T[] keys = map.KeySet();
+            T min = keys[0];
+            foreach (var elem in keys)
+            {
+                if (elem.CompareTo(min) < 0)
+                    min = elem;
+            }
+            return min;
+        }
+
+        public T? Last()
+        {
+            if (IsEmpty())
+                return default;
+            T[] keys = map.KeySet();
+            T max = keys[0];
+            foreach (var elem in keys)
+            {
+                if (elem.CompareTo(max) > 0)
+                    max = elem;
+            }
+            return max;
+        }
+
+        public bool IsEmpty() => size == 0;
+
+        public int Size() => size;
+
+        public T[] SubSet(T start, T stop)
+        {
+            bool flag = false;
+            T[] keys = map.KeySet();
+            int starts = 0, stops = 0;
+            for (int i = 0; i < keys.Length; i++)
+            {
+                if (keys[i].CompareTo(start) == 0)
+                    starts = i;
+                else if (keys[i].CompareTo(stop) == 0)
+                    stops = i;
+            }
+            if (starts == 0 && stops == 0)
+                throw new IndexOutOfRangeException("SubSet Error index Start and Stop is not found in set");
+            T[] result = new T[Math.Abs(starts - stops)];
+            int j = 0;
+            foreach (var elem in keys)
+            {
+                if (flag)
+                    result[j] = elem;
+                if (elem.CompareTo(start) == 0)
+                    flag = true;
+                if (elem.CompareTo(stop) == 0)
+                    flag = false;
+            }
+            return result;
+        }
+
+        public T[] HeadSet(T toElement)
+        {
+            T[] keys = map.KeySet();
+            int i = 0;
+            foreach (var elem in keys)
+            {
+                if (elem.CompareTo(toElement) < 0)
+                    i++;
+            }
+            T[] result = new T[i];
+            i = 0;
+            foreach (var elem in keys)
+            {
+                if (elem.CompareTo(toElement) < 0)
+                {
+                    result[i] = elem;
+                    i++;
+                }
+            }
+            return result;
+        }
+
+        public T[] TailSet(T toElement)
+        {
+            T[] keys = map.KeySet();
+            int i = 0;
+            foreach (var elem in keys)
+            {
+                if (elem.CompareTo(toElement) > 0)
+                    i++;
+            }
+            T[] result = new T[i];
+            i = 0;
+            foreach (var elem in keys)
+            {
+                if (elem.CompareTo(toElement) > 0)
+                {
+                    result[i] = elem;
+                    i++;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ lab 23/ConsoleApp5/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace lab23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Тестирование MyHashSet ===");
+
+            // Создание множества
+            var mySet = new MyHashSet<int>();
+
+            // Добавление элементов
+            Console.WriteLine("\nДобавляем элементы: 1, 3, 5, 7, 9");
+            mySet.Add(1);
+            mySet.Add(3);
+            mySet.Add(5);
+            mySet.Add(7);
+            mySet.Add(9);
+            mySet.Print();
+
+            // Проверка Contains
+            Console.WriteLine($"\nContains(3): {mySet.Contains(3)}");
+            Console.WriteLine($"Contains(10): {mySet.Contains(10)}");
+
+            // Удаление элемента
+            Console.WriteLine("\nУдаляем элемент 5");
+            mySet.RemoveAll(new[] { 5 });
+            mySet.Print();
+
+            // Получение первого и последнего элемента
+            Console.WriteLine($"\nFirst: {mySet.First()}");
+            Console.WriteLine($"Last: {mySet.Last()}");
+
+            // Добавление массива элементов
+            Console.WriteLine("\nДобавляем массив элементов: 10, 15, 20");
+            mySet.AddAll(new[] { 10, 15, 20 });
+            mySet.Print();
+
+            // Подмножество
+            Console.WriteLine("\nПодмножество от 3 до 15");
+            var subset = mySet.SubSet(3, 15);
+            Console.WriteLine("Subset: " + string.Join(", ", subset));
+
+            // HeadSet
+            Console.WriteLine("\nHeadSet (меньше 10)");
+            var headset = mySet.HeadSet(10);
+            Console.WriteLine("HeadSet: " + string.Join(", ", headset));
+
+            // TailSet
+            Console.WriteLine("\nTailSet (больше 10)");
+            var tailset = mySet.TailSet(10);
+            Console.WriteLine("TailSet: " + string.Join(", ", tailset));
+
+            // Проверка на пустоту
+            Console.WriteLine($"\nIsEmpty: {mySet.IsEmpty()}");
+
+            // Очистка множества
+            Console.WriteLine("\nОчищаем множество");
+            mySet.Clear();
+            Console.WriteLine($"IsEmpty после очистки: {mySet.IsEmpty()}");
+        }
+    }
+}
